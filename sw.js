d6786279fd00eb11322aa33bc19dab77c355f2ea
Marco Alvim <97541090+marcoalvim999@users.noreply.github.com>
2025-06-06 23:53:39 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('dotgen-v1').then(cache => cache.addAll(['./', './manifest.json'])));
-  self.skipWaiting();
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
